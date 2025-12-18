be91ee61423efe30a7e2cfee27614f0078bcc5b3
TimTmi <khoanart05@gmail.com>
2025-12-18 13:12:02 +0700

    change enums to varchar

--- src/main/java/com/example/chatroomserver/repository/LoginHistoryRepository.java
+++ src/main/java/com/example/chatroomserver/repository/LoginHistoryRepository.java
@@ -1,10 +1,11 @@
 package com.example.chatroomserver.repository;
 
 import com.example.chatroomserver.entity.LoginHistory;
+import com.example.chatroomserver.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface LoginHistoryRepository extends JpaRepository<LoginHistory, Integer> {
     List<LoginHistory> findAllByOrderByLoginTimeDesc();
-    List<LoginHistory> findByUsernameOrderByLoginTimeDesc(String username);
+    List<LoginHistory> findByUserOrderByLoginTimeDesc(User user);
 }
